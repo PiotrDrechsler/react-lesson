f45e21b45a4ce1a0b103425bc24ac3e4935f1c68
PiotrDrechsler <pdrech@gmail.com>
2024-05-04 18:27:27 +0200

    Target blank and stay on page
    Create page open in new tab in one link component, in second component stay at page
    
    1. create new props with true/fasle
    2. create const with ? to open new card or stay in page
    2. create rel const same as target const

--- react lesson/src/components/Link/Link.jsx
+++ react lesson/src/components/Link/Link.jsx
@@ -1,7 +1,9 @@
 export function Link(props) {
   const adress = "https://zrozumiecreact.pl";
+  const target = props.shouldOpenNewTab ? "_blank" : "";
+  const rel = props.shouldOpenNewTab ? "noopener noreffer" : "";
   return (
-    <a href={adress} target="_blank" rel="noopener noreffer">
+    <a href={adress} target={target} rel={rel}>
       {props.text}
     </a>
   );
