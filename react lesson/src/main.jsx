87afa0545a05477b9e896db8c8c4f4283240438a
PiotrDrechsler <pdrech@gmail.com>
2024-05-04 17:28:38 +0200

    bacic structure
    1.create element with heading, links and paragraf
    2.create link element with link, and use it as a const in element

--- react lesson/src/main.jsx
+++ react lesson/src/main.jsx
@@ -1,5 +1,26 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
-const element = <div></div>;
+const linkElement = (
+  <a href="https://zrozumiecreact.pl" target="_blank" rel="noopener noreffer">
+    Sprawdź już teraz!
+  </a>
+);
+
+const element = (
+  <>
+    <h1>Nasza Strona!</h1>
+    <main>
+      {linkElement}
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima soluta
+        inventore repudiandae autem commodi culpa tempora at sequi asperiores,
+        harum accusantium, eaque quo nemo numquam natus reiciendis dolorum esse
+        rem.
+      </p>
+      {linkElement}
+    </main>
+  </>
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(element);
