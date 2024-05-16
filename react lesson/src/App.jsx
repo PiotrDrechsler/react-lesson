d28f5431461933dbe64691d5a3fb5cd7f7bc6c2b
PiotrDrechsler <pdrech@gmail.com>
2024-05-16 21:33:01 +0200

    create/object "jan"
    1.create object "jan" in const.
    2. in object create: name, lastName, email and phone number
    3. return object "jan" in app function:
    -create <adress>

--- react lesson/src/App.jsx
+++ react lesson/src/App.jsx
@@ -1,5 +1,27 @@
+import React from "react";
+
 function App() {
-  return <div>Test</div>;
+  const jan = {
+    name: "Jan",
+    lastName: "Kowalski",
+    mail: "j.kow@gmail.com",
+    tel: "123-456-789",
+  };
+  return (
+    <address>
+      <div>
+        <span>
+          {jan.name} {jan.lastName}
+        </span>
+      </div>
+      <div>
+        Email: <a href={`mailto:${jan.mail}`}>{jan.mail}</a>
+      </div>
+      <div>
+        Tel: <a href={`tel:${jan.tel}`}>{jan.tel}</a>
+      </div>
+    </address>
+  );
 }
 
 export default App;
