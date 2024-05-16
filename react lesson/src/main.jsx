e2962feedbaad753bac9da2f82295e130a172844
PiotrDrechsler <pdrech@gmail.com>
2024-05-16 21:18:44 +0200

    Clean
    Clean files for next lesson

--- react lesson/src/main.jsx
+++ react lesson/src/main.jsx
@@ -1,44 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import { Link } from "./components/Link/Link";
-import { Animal } from "./components/Animal/Animal";
-import { HeadingItalic } from "./components/HeadingItalic/HeadingItalic";
-import { Logger } from "./components/Logger/Logger";
-import { TestComponent } from "./components/TestComponent";
-
-const element = (
-  <>
-    <h1>TEST</h1>
-    <TestComponent />
-    {/*
-    <Logger>
-      <HeadingItalic>Nasza Super Strona</HeadingItalic>
-    </Logger>
-    <main>
-      <Link text="Sprawdź już teraz!" shouldOpenNewTab={true} />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima soluta
-        inventore repudiandae autem commodi culpa tempora at sequi asperiores,
-        harum accusantium, eaque quo nemo numquam natus reiciendis dolorum esse
-        rem.
-      </p>
-      <Link shouldOpenNewTab={false} />
-      <h1>Galeria zwierząt</h1>
-      <Animal
-        name="Pies"
-        imgSrc="https://zrozumiecreact.pl/dog.jpg"
-        imgWidth={300}
-      />
-      <Animal
-        name="Kot"
-        imgSrc="https://zrozumiecreact.pl/cat.jpg"
-        imgWidth={350}
-      />
-      <Animal name="Kaczka" imgSrc="https://zrozumiecreact.pl/duck.jpg" />
-    </main>
-    */}
-  </>
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(element);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
