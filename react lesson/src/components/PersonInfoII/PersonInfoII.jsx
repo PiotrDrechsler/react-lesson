f89d8d2c55e2491292733df780785fef54b7af56
PiotrDrechsler <pdrech@gmail.com>
2024-05-17 16:02:23 +0200

    homwework/simple condition
    1.add logic with ternary operator: when age of user is >=18 show "Adult" in <span>
    2.change text to eng

--- react lesson/src/components/PersonInfoII/PersonInfoII.jsx
+++ react lesson/src/components/PersonInfoII/PersonInfoII.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+
 export function PersonInfoII({ person }) {
   return (
     <>
       <h2>{person.name}</h2>
       {/* Tutaj wyświetl h3 z napisem "Administrator" lub "Użytkownik" */}
-      <h3>{person.isAdmin ? "Administrator" : "Użytkownik"}</h3>
+      <h3>{person.isAdmin ? "Admin" : "User"}</h3>
+      {/* Tutaj wyświetl span, jeśli osoba jest pełnoletnia */}
+      <span> {person.age >= 18 && "Adult"}</span>
     </>
   );
 }
