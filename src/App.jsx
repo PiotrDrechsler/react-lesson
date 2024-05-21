955b125d7777339e310fa2b0c3b48f6169789199
PiotrDrechsler <pdrech@gmail.com>
2024-05-21 20:17:11 +0200

    basic structure
    Create basic structure for next lessons

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,20 @@
 import React from "react";
 
-import { PersonInfo } from "./components/Fundamentals/PersonInfo/PersonInfo";
-import { PersonInfoII } from "./components/Fundamentals/PersonInfoII/PersonInfoII";
-
-import dogImg from "./assets/dog.jpg";
-
 function App() {
-  const andrzej = {
-    name: "Jan",
-    lastName: "Kowalski",
-    mail: "jan.kow@gmail.com",
-    tel: "123-456-789",
-  };
+  const buttonEl = <button>Pokaż spoiler</button>;
 
-  const janina = {
-    name: "Janina",
-    lastName: "Kowalska",
-    mail: "janina.kow@gmail.com",
-    tel: "123-456-111",
-  };
-
-  const dio = {
-    name: "Dio",
-    lastName: "Kowalski",
-    mail: "dio.kow@gmail.com",
-  };
-
-  const joe = { name: "Joe", age: 21, isAdmin: false, country: "AU" };
-  const jan = { name: "Jan", age: 43, isAdmin: true, country: "PL" };
-  const juan = { name: "Juan", age: 15, isAdmin: true };
-  const jonas = { name: "Jonas", age: 18, isAdmin: false, country: "GER" };
+  // document.querySelector("button").addEventListener("click", () => {
+  //     alert("Klik");
+  // });
 
   return (
     <>
-      <img src="/images/dog.jpg" alt="dog" />
-      <PersonInfo person={janina} />
-      <br />
-      <PersonInfo person={andrzej} />
-      <br />
-      <PersonInfo person={dio} />
-      <br />
-      <PersonInfoII person={joe} />
-      <br />
-      <PersonInfoII person={jan} />
-      <br />
-      <PersonInfoII person={juan} />
-      <br />
-      <PersonInfoII person={jonas} />
-      <img src={dogImg} alt="dog" />
+      <h1>Gwiezdne wojny V</h1>
+      <h2>Rok produkcji: 1980</h2>
+      <h2>Fabuła</h2>
+      <p>Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.</p>
+      {buttonEl}
+      <p>Vader okazuje się być ojcem Luka.</p>
     </>
   );
 }
